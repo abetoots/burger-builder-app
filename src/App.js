70426e1bde16b9469641253be0d9456382d8156c
Abe Caymo <caymo.abesuni@gmail.com>
2019-07-13 21:10:13 +0800

    guarded routes by checking if authenticated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Logout/Logout';
+
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 class App extends Component {
   render() {
+
+    let route = (
+      <Switch>
+        <Route path="/auth" exact component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      route = (
+        <Switch>
+          <Route path="/auth" exact component={Auth} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" exact component={Orders} />
+          <Route path="/logout" exact component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
 
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" exact component={Orders} />
-            <Route path="/auth" exact component={Auth} />
-            <Route path="/logout" exact component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {route}
         </Layout>
       </div>
     );
   };
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    initCheckAuthentication: () => dispatch(actions.checkAuthentication())
+  }
+}
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
